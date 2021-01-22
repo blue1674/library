91a4d32418353952dc3bb821f4ea9e97d14b42ef
Manaf <f20190351@pilani.bits-pilani.ac.in>
2021-01-22 13:16:04 +0530

    Firebase functionality added but not secure

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-let myLibrary = [];
 const addButton = document.getElementById("addButton");
 const submitButton = document.getElementById("submitButton");
 const container = document.getElementsByClassName("container")[0];
 const form = document.getElementById("addBookForm");
 const closeIcon = document.getElementById('closeIcon');
 
-
-
 addButton.addEventListener("click", showForm);
-submitButton.addEventListener('click', submitForm);
 form.addEventListener('submit', handleForm);
 closeIcon.addEventListener('click', hideForm);
-
-function handleForm(event) { event.preventDefault(); }
 
 
 function Book(author, title, noOfPages, isRead) {
@@ -20,65 +14,72 @@ function Book(author, title, noOfPages, isRead) {
         this.title = title,
         this.noOfPages = noOfPages,
         this.isRead = isRead,
-    this.info = function () {
-        return `${title} by ${author}, ${noOfPages}, ${isRead}.`;
-    }
+        this.info = function () {
+            return `${title} by ${author}, ${noOfPages}, ${isRead}.`;
+        }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
 
-function display(book) {
+function display(doc) {
     const div = document.createElement("div");
+    const book = doc.data();
     div.innerHTML = `<p>Title: ${book.title}</p><p>Author: ${book.author}</p><p>Pages:  ${book.noOfPages}</p><p>Read?  ${book.isRead}</p>`;
     div.classList.add('card');
+    div.setAttribute('data-id', doc.id);
     container.appendChild(div);
     const deleteBook = document.createElement('img');
     deleteBook.src = "images/delete-book.png";
     deleteBook.classList.add('deleteBook');
-    deleteBook.addEventListener('click', () => {
-        let parent = deleteBook.parentNode;
-        let grand_father = parent.parentNode;
-        grand_father.removeChild(parent);
-        const index = myLibrary.indexOf(book);
-        myLibrary.splice(index, 1);
+    deleteBook.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('books').doc(id).delete();
     });
     div.prepend(deleteBook);
 }
 
-function displayStored() {
-    myLibrary.forEach(book => {
-        display(book);
-    });
-}
-for (let i = 1; i < 11; i++)
-    addBookToLibrary(new Book("JRR Tolkien", "The Hobbit", "295", "No"));// "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-displayStored();
-
-function displayRecent() {
-    display(myLibrary[myLibrary.length - 1]);
-
-}
 
 function showForm() {
     form.style.display = "flex";
 }
 
-function submitForm() {
-    const title = document.getElementById("title").value;
-    const author = document.getElementById('author').value;
-    const noOfPages = document.getElementById('noOfPages').value;
-    const read = document.querySelector('input[name="isRead"]:checked').value;
-    const book = new Book(author, title, noOfPages, read);
-    addBookToLibrary(book);
-    hideForm();
-    form.reset();
-    displayRecent();
-}
 
 
 
 function hideForm() {
     document.getElementById("addBookForm").style.display = "none";
 }
+
+    // // geting data
+    // db.collection('books').get().then((snapshot) => {
+    //     snapshot.docs.forEach(doc => {
+    //         display(doc.data(), doc);
+    //     })
+    // })
+
+// saving data
+function handleForm(event) {
+    event.preventDefault();
+    db.collection('books').add({
+        title: form.title.value,
+        author: form.author.value,
+        noOfPages: form.noOfPages.value,
+        isRead: form.isRead.value,
+    });
+    hideForm();
+}
+
+
+// real time listener
+db.collection('books').orderBy('title').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    console.log(changes);
+    changes.forEach(change => {
+        if(change.type == 'added'){
+            display(change.doc);
+        }else if(change.type == 'removed'){
+            let div = container.querySelector('[data-id='+ change.doc.id + ']');
+            container.removeChild(div);
+        }
+    })
+})
